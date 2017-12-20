bbcfc2e690fda6b364808e622468baca364f8384
Денис <kennetfreeman2337@gmail.com>
2017-12-20 21:14:16 +1000

    Greeting by login

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -4,6 +4,22 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    setlocale(LC_ALL, "rus");
+string name;
+cout << "login: ";
+	while(true)
+	{
+		getline(cin, name);
+
+		if(!cin)
+		{
+			cout << "login incorrect\n";
+			cin.clear();
+			 while(cin.get() !='\n');
+		}
+			else break;
+	}
+    cout << "Hello, " << name << endl;
+
     return 0;
 }
