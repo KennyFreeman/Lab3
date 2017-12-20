656d1e3964e08300f6b64bfafedaf218d1da4d31
Денис <kennetfreeman2337@gmail.com>
2017-12-20 21:20:45 +1000

    Adding Logins

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 
 using namespace std;
+#include <vector>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <locale.h>
+#include <algorithm>
+
+vector<string> new_users()
+{
+	vector<string> users;
+	users.push_back("Denis");
+	users.push_back("Vladimir");
+ return users;
+}
 
 int main()
 {
     setlocale(LC_ALL, "rus");
 string name;
+
+vector<string> users = new_users();
 cout << "login: ";
 	while(true)
 	{
@@ -13,13 +30,19 @@ cout << "login: ";
 
 		if(!cin)
 		{
-			cout << "login incorrect\n";
+			cout << "login incorrect" << endl;
 			cin.clear();
-			 while(cin.get() !='\n');
+			while (cin.get() != '\n');
 		}
 			else break;
 	}
-    cout << "Hello, " << name << endl;
+    	if (find(users.begin(), users.end(), name) != users.end()) {
+		cout << "Hello, " << name << "!" << endl;
+	} else {
+		cout << "Hello, Guest!" << endl;
+	}
 
+
+    system("pause");
     return 0;
 }
